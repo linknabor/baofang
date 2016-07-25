58f8302b65d1dbcc28a0f4283fde104258bc2f06
jackie <liuzj_jj@163.com>
2016-07-25 10:34:30 +0800

    合并宝房停车模块

--- common/src/main/java/com/yumu/hexie/integration/wuye/resp/BillListVO.java
+++ common/src/main/java/com/yumu/hexie/integration/wuye/resp/BillListVO.java
@@ -7,10 +7,11 @@ import com.yumu.hexie.integration.wuye.vo.BillInfo;
 
 public class BillListVO implements Serializable {
 
-	private static final long serialVersionUID = -3218697946940987315L;
-	
+	private static final long serialVersionUID = 1592900991660863594L;
+
 	private int total_count;
 	private List<BillInfo> bill_info;
+	private List<BillInfo> car_bill_info;
 	private int permit_skip_pay;//0?
 	private int permit_skip_car_pay;
 	private String meet_the_number;
@@ -54,6 +55,11 @@ public class BillListVO implements Serializable {
 	public void setPermit_skip_pay(int permit_skip_pay) {
 		this.permit_skip_pay = permit_skip_pay;
 	}
-	
+	public List<BillInfo> getCar_bill_info() {
+		return car_bill_info;
+	}
+	public void setCar_bill_info(List<BillInfo> car_bill_info) {
+		this.car_bill_info = car_bill_info;
+	}
 	
 }
