ad2e8d967bacfa5ce0fb23342b5652d620c54abd
liuzj_jj@163.com <liuzj_jj@163.com>
2018-10-29 11:29:40 +0800

    填掉baofang-wechatredis密码

--- hexiebk/src/main/java/com/yumu/hexie/common/config/AppConfig.java
+++ hexiebk/src/main/java/com/yumu/hexie/common/config/AppConfig.java
@@ -69,10 +69,6 @@ public class AppConfig {
     private String redisHost;
     @Value(value = "${redis.port}")
     private String redisPort;
-    @Value(value = "${redis.password}")
-    private String redisPassword;
-    @Value(value = "${redis.database}")
-    private int redisDatabase;
 
 	@Value(value = "${mainRedis.host}")
     private String mainRedisHost;
@@ -175,8 +171,6 @@ public class AppConfig {
         JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
         connectionFactory.setHostName(redisHost);
         connectionFactory.setPort(Integer.valueOf(redisPort));
-        connectionFactory.setPassword(redisPassword);
-        connectionFactory.setDatabase(redisDatabase);
         connectionFactory.setUsePool(true);
         return connectionFactory;
     }
